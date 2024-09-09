cf54b1aaf02fbd624c38548b44509c8a927bb2f2
Alvaro Signorio <90202428+Alvaro1251@users.noreply.github.com>
2024-09-09 02:53:51 -0300

    Delete src directory

--- src/timestamp/timestamp.middleware.spec.ts
+++
@@ -1,7 +0,0 @@
-import { TimestampMiddleware } from './timestamp.middleware';
-
-describe('TimestampMiddleware', () => {
-  it('should be defined', () => {
-    expect(new TimestampMiddleware()).toBeDefined();
-  });
-});
